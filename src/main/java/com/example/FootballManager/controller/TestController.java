58c9b34b01d9231859def676d8f843ed494b5722
SM4rtinho <sebastianmarut2@gmail.com>
2023-06-12 17:01:12 +0200

    putMapping and UserNotFoundException added

---
+++ src/main/java/com/example/FootballManager/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.FootballManager.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test-controller")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("zabezpieczony punkt");
+    }
+}
