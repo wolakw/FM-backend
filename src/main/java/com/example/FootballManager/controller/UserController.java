b1d37766e59687cb1d13b3bfd3940a67052a2f4d
SM4rtinho <sebastianmarut2@gmail.com>
2023-06-12 17:00:52 +0200

    putMapping and UserNotFoundException added

--- src/main/java/com/example/FootballManager/controller/UserController.java
+++ src/main/java/com/example/FootballManager/controller/UserController.java
@@ -26,7 +26,6 @@ public class UserController {
     @PutMapping("/user/{id}")
     User updateUser(@RequestBody User newUser,@PathVariable Long id){
         return userRepository.findById(id).map(user -> {
-            user.setUsername(newUser.getUsername());
             user.setName(newUser.getName());
             user.setEmail(newUser.getEmail());
             return userRepository.save(user);
--- src/main/java/com/example/FootballManager/model/User.java
+++ src/main/java/com/example/FootballManager/model/User.java
@@ -1,18 +1,33 @@
 package com.example.FootballManager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 
 @Entity
-public class User {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
     private Long id;
-    private String username;
     private String name;
     private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
 
     public Long getId() {
         return id;
@@ -22,13 +37,41 @@ public class User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    @Override
+    public String getPassword() {
+        return password;
     }
+
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 
     public String getName() {
         return name;
